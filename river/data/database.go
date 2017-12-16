990935cc90d9e080a72bfe5e4f5a7afae85cc2d9
Joshua Hawxwell <m@hawx.me>
2017-12-16 16:10:08 +0000

    Remove unused database stuff

--- river/data/database.go
+++ river/data/database.go
@@ -9,63 +9,12 @@ import (
 
 // Database is a key-value store with data arranged in buckets.
 type Database interface {
+	// Feed returns a database for storing known items from a named feed.
 	Feed(name string) (feed.Database, error)
 
+	// Confluence returns a database for storing past rivers.
 	Confluence() (confluence.Database, error)
 
-	// Bucket returns a namespaced bucket for storing key-value data.
-	Bucket(name []byte) (Bucket, error)
-
-	// Close releases all database resources. All transactions must be complete
-	// before closing.
+	// Close releases all database resources.
 	Close() error
-}
-
-// Bucket is a table for storing key-value pairs.
-type Bucket interface {
-	// View executes the function in the context of a read-only transaction.
-	View(func(ReadTx) error) error
-
-	// Update executes the function in the context of a read-write transaction.
-	Update(func(Tx) error) error
-}
-
-// ReadTx is a read-only database transaction.
-type ReadTx interface {
-	// Get returns the value associated with a key. Returns a nil value if the key does not exist.
-	Get(key []byte) []byte
-
-	// After returns all values listed after the key given, inclusive, to the last
-	// value, in sorted key order.
-	After(first []byte) [][]byte
-
-	// KeysBefore returns all keys from the first to the key given, exlusive, in
-	// sorted key order.
-	KeysBefore(last []byte) [][]byte
-
-	// All returns all values listed in sorted key order.
-	All() [][]byte
-}
-
-// Tx is a database transaction.
-type Tx interface {
-	// Get returns the value associated with a key. Returns a nil value if the key does not exist.
-	Get(key []byte) []byte
-
-	// Put sets the value associated with a key.
-	Put(key, value []byte) error
-
-	// Delete removes the value associated with a key.
-	Delete(key []byte) error
-
-	// After returns all values listed after the key given to the last value, in
-	// sorted key order.
-	After(start []byte) [][]byte
-
-	// KeysBefore returns all keys from the first to the key given, exlusive, in
-	// sorted key order.
-	KeysBefore(last []byte) [][]byte
-
-	// All returns all values listed in sorted key order.
-	All() [][]byte
 }
